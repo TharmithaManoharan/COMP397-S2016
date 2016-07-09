62e67450d412ecffb5988f4684ea19507d7c5955
TharmithaManoharan <tmanoha4@my.centennialcollege.ca>
2016-07-09 02:15:30 -0400

    Added tsd support for creatjs

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -1,19 +1,3 @@
-var objects;
-(function (objects) {
-    var Person = (function () {
-        //constructors
-        function Person(name, age) {
-            this._name = name;
-            this._age = age;
-        }
-        //public methods
-        Person.prototype.Speaks = function () {
-            console.log(this._name + " says hello!");
-        };
-        return Person;
-    }());
-    objects.Person = Person;
-})(objects || (objects = {}));
 function init() {
     console.log("Game started now!");
 }
--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -1,19 +1,4 @@
-module objects{
-export class Person{
-	//private instance variables
-	private _name: string;
-	private _age: number;
-	//constructors
-	constructor(name, age){
-		this._name=name;
-		this._age=age;
-	}
-	//public methods
-	public Speaks():void{
-		console.log(this._name + " says hello!");
-	}
-}
-}
+
 
 function init():void {
     console.log("Game started now!");
